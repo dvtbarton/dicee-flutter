36b6a865c79d2d83470506320bb9f68998474f56
Todd Barton <dvtbarton@gmail.com>
2021-01-09 21:14:00 -0700

    Finished!

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,15 +11,58 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: DiceePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DiceePage extends StatefulWidget {
+  @override
+  _DiceePageState createState() => _DiceePageState();
+}
+
+class _DiceePageState extends State<DiceePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                randoDice();
+                print('Left button pressed');
+              },
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png', /* using $variable is string interpolation */
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                randoDice();
+                print('Right button pressed');
+              },
+              child: Image.asset(
+                'images/dice$rightDiceNumber.png', /* using $variable is string interpolation */
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void randoDice() {
+    setState(() {
+      /* setState() calls build() to refresh screen */
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
